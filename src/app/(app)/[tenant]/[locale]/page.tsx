cc5befb0fa39d3890db7924057d71decdca77d42
Dalton <daltonkock@GMAIL.COM>
2025-05-19 14:55:17 -0500

    Update page.tsx

--- src/app/(app)/[tenant]/[locale]/page.tsx
+++ src/app/(app)/[tenant]/[locale]/page.tsx
@@ -1,20 +1,3 @@
 import Page from './[...slug]/page'
 
-import { notFound, redirect } from 'next/navigation'
-import { getPayload } from 'payload'
-import configPromise from '@payload-config'
-import React from 'react'
-
-export default async function TenantLocalePage({
-  params,
-}: {
-  params: { tenant: string; locale: string }
-}) {
-  try {
-    // Redirect to the home page for this tenant/locale
-    return redirect(`/${params.locale}/home`);
-  } catch (error) {
-    console.error('Error in tenant locale page:', error);
-    return <div>Error loading tenant home page</div>;
-  }
-}
+export default Page
